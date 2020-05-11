d3a2058475e7bb0dd3107fd9ae44531267e924a7
Fernando Chamorro <joseferchamorro@gmail.com>
2020-05-10 21:56:21 -0300

    API - Instalación y login

---
+++ api/routes/api.php
@@ -0,0 +1,57 @@
+<?php
+/** @var Router $router */
+
+use Laravel\Lumen\Routing\Router;
+
+/* Public Routes */
+$router->get('/', function () {
+    return response()->json(['message' => 'Bienvenido a la API del sistema de turnos de Grupo SIM']);
+});
+
+/* Auth Routes */
+$router->group(['prefix' => 'auth', 'as' => 'auth'], function (Router $router) {
+
+    /* Defaults */
+    $router->post('/register', [
+        'as' => 'register',
+        'uses' => 'AuthController@register',
+    ]);
+    $router->post('/login', [
+        'as' => 'login',
+        'uses' => 'AuthController@login',
+    ]);
+    $router->get('/verify/{token}', [
+        'as' => 'verify',
+        'uses' => 'AuthController@verify'
+    ]);
+
+    /* Password Reset */
+    $router->post('/password/forgot', [
+        'as' => 'password.forgot',
+        'uses' => 'AuthController@forgotPassword'
+    ]);
+    $router->post('/password/recover/{token}', [
+        'as' => 'password.recover',
+        'uses' => 'AuthController@recoverPassword'
+    ]);
+
+    /* Protected User Endpoint */
+    $router->get('/user', [
+        'uses' => 'AuthController@getUser',
+        'as' => 'user',
+        'middleware' => 'auth'
+    ]);
+});
+
+/* Protected Routes */
+$router->group(['middleware' => 'auth'], function (Router $router) {
+
+    $router->group(['middleware' => 'role:administrator'], function (Router $router) {
+
+        $router->get('/admin', function () {
+            return response()->json(['message' => 'Usted es un administrador.']);
+        });
+
+    });
+
+});
